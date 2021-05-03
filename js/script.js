ac33570d49c0486c2402a108b45717f627322772
GioOli7 <gio07live@gmail.com>
2021-05-03 16:09:48 +0200

    email list

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        emailList: [],
+    },
+    created() {
+        this.getMail(10);
+        // console.log(this.emailList);
+    },
+    
+    methods: {
+
+        /**
+         * 
+         * @param {number} nElem number of emails to generate
+         */
+        getMail(nElem) {
+            for (let i = 0; i < nElem; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((result) => {
+                    this.emailList.push(result.data.response)
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+            }
+        },
+
+        isReady() {
+            return this.emailList.length === 10;
+        }
+
+    }
+})
